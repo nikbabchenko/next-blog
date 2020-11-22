5bb26fe93d1d606426938c8e8efeb8d1e4b35bd4
Mykola Babchenko <mykolababchenko@truckstop.com>
2020-11-22 16:58:22 +0200

    fixes comments url

--- components/disqus-comments/disqus-comments.js
+++ components/disqus-comments/disqus-comments.js
@@ -3,7 +3,7 @@ import {DiscussionEmbed} from "disqus-react";
 export const DisqusComments = ({ post }) => {
   const disqusShortname = "Nick's tech blog"
   const disqusConfig = {
-    url: `https://nick-babchenko-tech.disqus.com/${post.title}`,
+    url: `https://http-nick-babchenko-tech.disqus.com/${post.title}`,
     identifier: post.id, // Single post id
     title: post.title // Single post title
   }
